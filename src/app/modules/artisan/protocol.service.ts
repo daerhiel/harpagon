07881f336790748b7929cd6a1fc232b1048c2dcd
daerhiel <485120+daerhiel@users.noreply.github.com>
2023-11-07 23:50:43 +0100

    feature(Protocol): Add local storage recipe caching

--- src/app/modules/artisan/protocol.service.ts
+++ src/app/modules/artisan/protocol.service.ts
@@ -3,6 +3,7 @@ import { toObservable } from '@angular/core/rxjs-interop';
 import { EMPTY, Observable, expand, filter, from, last, map, mergeMap, of, switchMap, toArray } from 'rxjs';
 
 import { Ingredient, NwDbApiService, Recipe } from "@modules/nw-db/nw-db.module";
+import { getStorageItem, setStorageItem } from '@app/services/settings';
 
 export interface RecipeMapping {
   recipe: Recipe;
@@ -15,20 +16,33 @@ export interface RecipeMapping {
 export class ProtocolService {
   readonly #nwDbApi: NwDbApiService = inject(NwDbApiService);
 
-  readonly #index: Record<string, Recipe> = {};
+  readonly #recipes: Record<string, Recipe> = {};
+
+  constructor() {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key && key.startsWith('recipe:')) {
+        const recipe = getStorageItem<Recipe | null>(key, null);
+        if (recipe) {
+          this.#recipes[recipe.id] = recipe;
+        }
+      }
+    }
+  }
 
   protected cacheAndGet(...recipes: Recipe[]): string[] {
     const ids: string[] = [];
 
     for (const recipe of recipes) {
       if (recipe) {
-        if (!(recipe.id in this.#index)) {
-          this.#index[recipe.id] = recipe;
+        if (!(recipe.id in this.#recipes)) {
+          this.#recipes[recipe.id] = recipe;
+          setStorageItem(`recipe:${recipe.id}`, recipe);
         }
 
         for (const ingredient of recipe.ingredients) {
           const ref = ingredient.recipeId;
-          if (ref && !(ref.id in this.#index) && !ids.includes(ref.id)) {
+          if (ref && !(ref.id in this.#recipes) && !ids.includes(ref.id)) {
             ids.push(ref.id);
           }
         }
@@ -59,8 +73,8 @@ export class ProtocolService {
       const indexer = (ingredients: Ingredient[]) => {
         for (const ingredient of ingredients) {
           const ref = ingredient.recipeId;
-          if (ref && ref.id in this.#index && !(ref.id in index)) {
-            const recipe = this.#index[ref.id];
+          if (ref && ref.id in this.#recipes && !(ref.id in index)) {
+            const recipe = this.#recipes[ref.id];
             index[ref.id] = recipe;
             indexer(recipe.ingredients);
           }
