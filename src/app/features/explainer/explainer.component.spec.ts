15165aa9e7bf7ca51411a7eb585c8d7d2d73aa44
daerhiel <485120+daerhiel@users.noreply.github.com>
2024-03-03 16:12:05 +0100

    feature(Specs): Add test providers to dialogs

--- src/app/features/explainer/explainer.component.spec.ts
+++ src/app/features/explainer/explainer.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { ExplainerComponent } from './explainer.component';
+
+function createDialogRef<T>(): MatDialogRef<T> {
+  return jasmine.createSpyObj<MatDialogRef<T>>('MatDialogRef', { close: undefined });
+}
 
 describe('ExplainerComponent', () => {
   let component: ExplainerComponent;
   let fixture: ComponentFixture<ExplainerComponent>;
+  let dialogRef: MatDialogRef<ExplainerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExplainerComponent]
+      imports: [ExplainerComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRef = createDialogRef() },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ExplainerComponent);
